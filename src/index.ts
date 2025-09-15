dcfefe17f85f590de156b9d9c78d2fdc44354e38
Herfando <herfando_91@yahoo.com>
2025-09-15 19:50:15 +0700

    edit button

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const display = document.getElementById("display") as HTMLInputElement;
+const buttons = document.querySelectorAll("button");
+
+let currentInput: string = "";
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const value = btn.textContent ?? "";
